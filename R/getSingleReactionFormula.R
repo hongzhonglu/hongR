103ed01a42f88fc184fedff2c27f95ca8620c95a
hongzhonglu <luho@chalmers.se>
2018-07-14 11:48:55 +0200

    feat: add single mapping function

--- R/getSingleReactionFormula.R
+++ R/getSingleReactionFormula.R
@@ -1,26 +1,25 @@
-#' get multiple mapping results for one element
-#'
+#' get single mapping result for one element
 #'
 #' @param description vector contains the description for each element from 'reaction'
 #' @param reaction vector contrains the element been mapped
 #' @param ko vector contains the element need mapping
 #'
-#' @return one element, which contains several mapping results seperated by ";"
+#' @return one element
 #' @export
 #'
 #' @examples
 #' w <- 1:6
 #' v <- c('z','a','b','a','b', 'e')
 #' testData <- c('a','b','g')
-#' getMultipleReactionFormula(w,v,testData)
+#' getSingleReactionFormula(w,v,testData)
 #'
-getMultipleReactionFormula <- function(description, reaction, ko) {###description can be any charater of metabolite
+getSingleReactionFormula <- function(description, reaction, ko) {###description can be any charater of metabolite
   index <- vector()
   result <- vector()
   tt <- vector()
   for (i in 1:length(ko)){
-    if(length( which (reaction %in%  ko[i]))){
-      index <- which (reaction %in%  ko[i])
+    if(length(match(ko[i],reaction))){
+      index <- match(ko[i],reaction)
       tt <- description[index]
       result[i] <- paste0(tt, collapse = ";")
     } else{
